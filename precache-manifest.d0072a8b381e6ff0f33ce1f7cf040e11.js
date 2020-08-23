00c47462349bf0be10fe15c17231db4d84335b7a
Khang Le Thanh <khang.lethanh@student.tut.fi>
2020-08-23 19:17:19 +0300

    Updates

---
+++ precache-manifest.d0072a8b381e6ff0f33ce1f7cf040e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8fdd9afa455da09c3fdc3f5f0429cb",
+    "url": "/wines/index.html"
+  },
+  {
+    "revision": "d6e1f891f5db305f5261",
+    "url": "/wines/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7089469e0684de218821",
+    "url": "/wines/static/js/2.da721d07.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wines/static/js/2.da721d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e1f891f5db305f5261",
+    "url": "/wines/static/js/main.ed428998.chunk.js"
+  },
+  {
+    "revision": "9a9b419f7d40f3b55597",
+    "url": "/wines/static/js/runtime-main.b858b653.js"
+  }
+]);
